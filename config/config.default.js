83258b5fa3b92b8a2df96bafedc5fa56357d893c
Camusama <a1012401749@163.com>
2018-12-29 16:57:48 +0800

    add remote

---
+++ config/config.default.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = appInfo => {
+  const config = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1545063188284_9966';
+
+  // add your config here
+  config.middleware = [ 'report', 'notfoundHandler' ];
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
+  config.news = {
+    pageSize: 5,
+    serverUrl: 'https://hacker-news.firebaseio.com/v0',
+  };
+  config.robot = {
+    ua: [ /curl/i, /Baiduspider/i ],
+  };
+  config.sequelize = {
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'egg-sequelize-doc-default',
+  };
+  return config;
+};
